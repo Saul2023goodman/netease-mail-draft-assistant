91d740d752c29de071c7eb4364a31c838428b3d1
Ceng Yong Han <124812663+Saul2023goodman@users.noreply.github.com>
2026-09-08 15:05:18 +0800

    refactor: centralize product automation defaults

--- default-policy.js
+++ default-policy.js
@@ -14,6 +14,16 @@
       skipHolidays: true,
       allowDomainFallback: false
     }),
+    followUp: Object.freeze({
+      mode: 'forward',
+      minDays: 7,
+      maxCount: 1,
+      blockHumanReply: true,
+      blockAutoReply: false,
+      fwPrefix: 'Fw:',
+      rePrefix: 'Re:',
+      template: 'Dear {{name}},\n\nI am writing to follow up on my previous email regarding {{subject}}. I would be grateful if you had a chance to review it.\n\nBest regards,'
+    }),
     identity: Object.freeze({
       aliases: Object.freeze([]),
       persistInferredAliases: false
